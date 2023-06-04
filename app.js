b4ee1575689803b99b9cb32755d11d0797f230fa
bricheek <bricheek@gmail.com>
2023-06-04 17:03:52 -0400

    using form to submit

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const submission = require('./routes/submission.js');
 const express = require('express');
 const app = express();
+const path = require('path');
 // app.urlencoded({extended:true})
 app.use(express.json());
 
-// display form on homepage - this should be moved to UI app eventually
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/form.html'));
-});
-
 // Listen to the App Engine-specified port, or 8080 otherwise
-const PORT = process.env.PORT || 8080;
+const PORT = parseInt(process.env.PORT) || 8080;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
+});
+
+// display form on homepage - this should be moved to UI app eventually
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/views/form.html'));
 });
 
 app.use('/', submission);
